20c28b8109bbcb74ad99eb3d292eb80e48d2cdd9
Andi Palo <sanandrea8080@gmail.com>
2016-10-11 12:23:24 +0200

    first commit

---
+++ FileCounter.Xamarin/ApiDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Foundation;
+
+
+namespace FileCounter.Xamarin
+{
+	// @interface FileCounter : NSObject
+	[BaseType(typeof(NSObject))]
+	interface FileCounter
+	{
+		// +(void)lsof;
+		[Static]
+		[Export("lsof")]
+		void Lsof();
+	}
+}
